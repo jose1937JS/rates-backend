40f836d283c4ff5f00149c625235a7a89acfb73f
Jose Lopez <jflo1937@gmail.com>
2025-09-13 13:21:24 -0400

    Refactor rate retrieval logic; add getTodayRates function to fetch and filter today's rates

--- src/routes/test.js
+++ src/routes/test.js
@@ -3,6 +3,55 @@ const router = express.Router();
 const Rate = require('../models/rates');
 const obtenerValoresBCV = require('../utils/getBCVvalues');
 
+const getTodayRates = async () => {
+  const today = new Date();
+
+  const startOfToday = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate()
+    );
+
+    const endOfToday = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate(),
+      23, 59, 59, 999
+    );
+    
+    const rates = await Rate.find({
+      createdAt: { 
+        $gte: startOfToday,
+        $lte: endOfToday
+      }
+    }, { 
+      rate: 1, 
+      currency: 1, 
+      _id: 0, 
+      name: 1,
+      createdAt: 1 
+    })
+    .sort({ createdAt: -1 })
+    .exec();
+
+    // Filtrar solo los rates requeridos
+    const filtered = rates.filter(r => ["YD_USD", "BCV_EUR", "BCV_USD"].includes(r.currency));
+
+    // Obtener el YD_USD más reciente
+    const ydUsd = filtered.find(r => r.currency === "YD_USD");
+
+    // Obtener el BCV_EUR más reciente
+    let bcvEur = filtered.find(r => r.currency === "BCV_EUR");
+
+    // Obtener el BCV_USD más reciente
+    let bcvUsd = filtered.find(r => r.currency === "BCV_USD");
+
+    // Solo devolver los que existan
+    const result = [ydUsd, bcvEur, bcvUsd].filter(Boolean)
+
+    return result;
+}
+
 router.get('/', async (req, res) => {
   const { euro: euroBVC, dollar: dollarBCV } =  await obtenerValoresBCV();
   let dollarYadioValue;
@@ -23,12 +72,18 @@ router.get('/', async (req, res) => {
 
     console.log(`Rate saved: Dollar Yadio: ${dollarYadioValue}`);
 
+    // get rates from db
+    const rates = await getTodayRates();
+
     return res.json({
       message: 'Rates updated successfully',
-      rates: { euroBVC, dollarBCV, dollarYadioValue }
+      rates
     });
   })
-  .catch(() => console.error('Error fetching Yadio rate'));
+  .catch((error) => {
+    console.error('Error fetching Yadio rate', error)
+    return res.status(500).json("Ha habido un error obteniendo los datos del USDT")
+  });
 });
 
 module.exports = router;
